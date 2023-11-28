f91b966e986cce3b1821587f6e1cac065b7192f9
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2023-11-28 20:03:33 +0100

    updated config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-    export const contractAddress = "0xddC7750B22683EB35E5A65eE7C2b12657fd89865"
-   export const ownerAddress = "0x4f5088782e3D8bE35A750e7B9b14d3F755210217"
+export const contractAddress = "0xddC7750B22683EB35E5A65eE7C2b12657fd89865"
+export const ownerAddress = "0x4f5088782e3D8bE35A750e7B9b14d3F755210217"
