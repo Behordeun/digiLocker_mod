27bc9f81e0cbd9bb2190d31591906326e3cd3eb5
Muhammad Abiodun SULAIMAN <108724938+BashyPy@users.noreply.github.com>
2023-11-29 07:24:43 +0100

    Create run_kubernetes.sh

---
+++ run_kubernetes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+#This is your Docker ID/path
+dockerpath=herbehordeun/digilocker-mod
+# Run the Docker Hub container with kubernetes
+
+kubectl run digilocker-mod\
+    --image=$dockerpath\
+    --port=80 --labels app=digilocker-mod
+
+
+# List kubernetes pods
+kubectl get pods
+
+# Forward the container port to a host
+kubectl port-forward digilocker-mod 8000:80
