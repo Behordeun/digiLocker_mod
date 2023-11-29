d84595c1db78d16cd7f629a1708f4f1ae510bb80
Muhammad Abiodun SULAIMAN <108724938+BashyPy@users.noreply.github.com>
2023-11-29 07:22:25 +0100

    Create upload_docker.sh

---
+++ upload_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=herbehordeun/digilocker_mod
+
+# Step 2:  
+# Authenticate & tag
+docker login --username=herbehordeun
+docker image tag digilocker_mod $dockerpath
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+docker push $dockerpath:latest
