abce3045c8921a7f08c7e8b1fd046067fd027f6e
Muhammad Abiodun SULAIMAN <108724938+BashyPy@users.noreply.github.com>
2023-11-29 07:20:31 +0100

    Create run_docker.sh

---
+++ run_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+docker build -t digilocker_mod:dev-2 .
+
+# Step 2: 
+docker images
+
+# Step 3:
+docker run -p 8083:8083 igilocker_mod:dev-2
