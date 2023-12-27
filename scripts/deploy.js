2873d6d115aab67df7d6df1b977da72d80686995
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2023-12-27 20:49:51 +0100

    🚨 Fix indentation in deploy script
    Corrected the alignment of the ownerAddress line in the deploy.js
    configuration export to maintain consistent code formatting and improve
    readability. This change ensures that the indentation matches the
    surrounding code structure.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -24,7 +24,7 @@ async function main() {
     "./config.js",
     `
     export const contractAddress = "${contractAddress}"
-   export const ownerAddress = "${ownerAddress}"
+    export const ownerAddress = "${ownerAddress}"
 `
   );
 }
