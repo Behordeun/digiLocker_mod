32265ee19e29351d15afb6065ad643f47a7e0e66
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2023-12-27 14:31:08 +0100

    Introduced dapr configuration into the application

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 from app import app
 import os
+import settings
 from dotenv import load_dotenv
+# from dapr.actor import ActorInterface, actormethod
+from dapr.actor.runtime.config import (
+    ActorRuntimeConfig,
+    ActorReentrancyConfig,
+)  # , ActorTypeConfig
+from flask_dapr.actor import DaprActor
+
+# from dapr.conf import settings
+from dapr_config.demo_actor import DemoActor
+
+
+# Enable DaprActor Extension
+actor = DaprActor(app)
+app.config.from_object(settings)
+
+# Register DemoActor
+actor.register_actor(DemoActor)
+
+# Create ActorRuntime configuration
+# actor_runtime_config = ActorRuntimeConfig(
+#    actor_idle_timeout=timedelta(hours=1),
+#    actor_scan_interval=timedelta(seconds=30),
+#    drain_ongoing_call_timeout=timedelta(minutes=1),
+#    drain_rebalanced_actors=True,
+#    reentrancy=ActorReentrancyConfig(enabled=False),
+#    reminders_storage_partitions=7
+# )
+
 
 load_dotenv()
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("APPLICATION_HOST"),
+        host=app.config["APPLICATION_HOST"],
         debug=True,
-        port=os.getenv("APPLICATION_PORT"),
+        port=app.config["APPLICATION_PORT"],
     )
