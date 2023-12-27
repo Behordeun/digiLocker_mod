794f51762a9c2a43eeb42e845b67a4d107ea215b
Muhammad Abiodun SULAIMAN <108724938+BashyPy@users.noreply.github.com>
2023-12-28 00:20:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import settings
 
 if __name__ == "__main__":
     app.run(
-        host=app.config["APPLICATION_HOST"],
+        host=app.config["APPLICATION_HOST", "0.0.0.0"],
         debug=True,
-        #port=app.config["APPLICATION_PORT"],
+        port=app.config["APPLICATION_PORT", 8083],
     )
