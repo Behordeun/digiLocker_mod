851e159b83ae16c597ec75d2abd749efc13c0ecc
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2024-01-27 01:09:28 +0100

    modified run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,7 +2,7 @@
 
 #This is your Docker ID/path
 dockerpath=herbehordeun/digilocker-mod
-imageTag=digilocker_heroku
+imageTag=latest
 
 # Run the Docker Hub container with kubernetes
 kubectl run digilocker-mod --image=$dockerpath:$imageTag --port=8083 --labels app=digilocker-mod
